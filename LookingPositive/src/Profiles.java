772a2bb860465a1f01d29f5207c7527d6e10ddc3
Chrisstamato01 <t8190236@aueb.gr>
2020-11-14 18:52:32 +0200

    Kalhspera

---
+++ LookingPositive/src/Profiles.java
@@ -0,0 +1,45 @@
+
+public class Profiles {
+	
+	private String firstName, lastName, mobilePhone, residenceRegion, userID, gender, dateOfInfection;
+	private int age;
+	private boolean isSusceptible;
+	private String[] closeContactWith;
+
+	public void modelCase(String firstName, String lastName, String mobilePhone, 
+					 String residenceRegion, String userID, String gender, 
+					 String dateOfInfection, int age, boolean isSusceptible, String[]closeContactWith) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.mobilePhone = mobilePhone;
+		this.residenceRegion = residenceRegion;
+		this.userID = userID;
+		this.gender = gender;
+		this.dateOfInfection = dateOfInfection;
+		this.age = age;
+		this.isSusceptible = isSusceptible;
+		this.closeContactWith = closeContactWith;
+		
+	}
+
+	public String getFirstName() { return firstName; }
+
+	public String getLastName() { return lastName; }
+
+	public String getMobilePhone() { return mobilePhone; }
+	
+	public String getResidenceRegion() { return residenceRegion; }
+
+	public String getUserID() { return userID; }
+
+	public String getGender() { return gender; }
+
+	public String getDateOfInfection() { return dateOfInfection; }
+
+	public int getAge() { return age; }
+
+	public boolean isSusceptible() { return isSusceptible; }
+
+	public String[] getCloseContactWith() { return closeContactWith; }
+
+}
