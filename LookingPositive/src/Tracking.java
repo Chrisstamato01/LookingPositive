5bafc476faf406b5107783e3f7a2566f6ae26900
Σοφία <sophiastr01@hotmail.com>
2020-12-23 02:09:59 +0200

    Συνδέσαμε με την μελένια την κλάση Tracking με τη mail και την Eventsaver

--- LookingPositive/src/Tracking.java
+++ LookingPositive/src/Tracking.java
@@ -1,30 +1,27 @@
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Set;
-
 public class Tracking {
-private ArrayList<Profiles> contactstoinform = new ArrayList<Profiles>();
-public  void track(Profiles a ) { //το α είναι το τρέχων αντικείμενο
+private static ArrayList<String> contactstoinform = new ArrayList<String>();
+public static ArrayList<String> newContacts = removeDuplicates(contactstoinform);
+public  void track(int userID) { //πρέπει να μου στέλνουν το userID
 	for (int k = 0 ; Eventsaver.fourteendays.length < 14; k++ ) {
-		for (int i = 0; i < Eventsaver.fourteendays[k].eventlist.size(); i++) {
-			if ( a.getUserID() == i) {
-			for (int j = 0; j < Eventsaver.fourteendays[k].eventlist.get(i).size(); j ++) {
-				for (int l = 0; l< Eventsaver.fourteendays[k].eventlist.get(i).get(j).size(); l++) {
-					contactstoinform.add(Eventsaver.fourteendays[k].eventlist.get(i).profiles.get(l).getemail();
-				}
+		for (int j = 0; j < Eventsaver.fourteendays[k].eventlist.get(userID).size(); j ++) {
+			for( int l = 0; l < Eventsaver.fourteendays[k].eventlist.get(userID).get(j).getProfiles().size(); l++) {
+				contactstoinform.add(Eventsaver.fourteendays[k].eventlist.get(userID).get(j).getProfiles().get(l).getEmail());
 			}
-		}
 		}
 	}
 }
 
-public <Profiles> ArrayList<Profiles> removeDuplicates(ArrayList<Profiles> list) {
-Set<Profiles> set = new LinkedHashSet<>();
+public static <String> ArrayList<String> removeDuplicates(ArrayList<String> list) {
+Set<String> set = new LinkedHashSet<>();
 set.addAll(list);
 list.clear();
 list.addAll(set);
 return list;
 
 }
-public ArrayList<Profiles> newContacts = removeDuplicates(contactstoinform);
 }
+
+//interator για να διατρέξεις τον πίνακα 
