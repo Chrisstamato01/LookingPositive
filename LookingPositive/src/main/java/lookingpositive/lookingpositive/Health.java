f6f66f702634cb9a4b3aa82a0ad96711fc6115fa
melenia <meleniaabebedeli@gmail.com>
2021-01-11 02:08:53 +0200

    Closed scanner in Health

--- LookingPositive/src/main/java/lookingpositive/lookingpositive/Health.java
+++ LookingPositive/src/main/java/lookingpositive/lookingpositive/Health.java
@@ -43,25 +43,32 @@ public final class Health {
     System.out.println(
         " Αν θέλετε να ενημερώσετε την κατάσταση υγεία σας, πιέστε 1 ");
     System.out.println(" Αν είστε θετικός στο Κορωνοϊό, πιέστε 2 ");
-    Scanner sc = new Scanner(System.in);
-    int answer = 0;
-    while (answer != 1 || answer != 2) {
-      try {
-        answer = sc.nextInt();
+    Scanner sc = null;
+    try {
+      sc = new Scanner(System.in);
+      int answer = 0;
+      while (answer != 1 || answer != 2) {
+        try {
+          answer = sc.nextInt();
 
-      } catch (Exception ex) {
-        sc.next();
-        System.out.println("Μη έγκυρη τιμή");
-      }
+        } catch (Exception ex) {
+          sc.next();
+          System.out.println("Μη έγκυρη τιμή");
+        }
 
-      if (answer != 1 || answer != 2) {
-        System.out.println("Εισάγετε επιλογή [1-2]");
+        if (answer != 1 || answer != 2) {
+          System.out.println("Εισάγετε επιλογή [1-2]");
+        }
       }
-    }
-    if (answer == 2) {
-      Tracking.track(userID);
-    } else {
-      areYouOk();
+      if (answer == 2) {
+        Tracking.track(userID);
+      } else {
+        areYouOk();
+      }
+    } finally {
+      if (sc != null) {
+        sc.close();
+      }
     }
   }
 
@@ -72,94 +79,101 @@ public final class Health {
    */
   public static void areYouOk() {
     System.out.println(" Νιώθετε καλά σήμερα ;(ΝΑΙ/ΟΧΙ) ");
-    Scanner sc = new Scanner(System.in);
-    String answer = null;
-    boolean flag = true;
-    while (flag) {
-      try {
-        answer = sc.next();
-        flag = false;
-      } catch (Exception e) {
-        flag = true;
-        System.out.println("Εισάγετε (ΝΑΙ/ΟΧΙ) ");
-      }
-    }
-    if (answer.equalsIgnoreCase("ΟΧΙ")) {
-      flag = false;
-      int countsymptoma = 0;
-      while (!flag) {
-        System.out.println("Άν έχετε πυρετό πατήστε 1");
-        System.out.println("Άν έχετε βήχα πατήστε 2");
-        System.out.println("Άν αισθάνεστε κόπωση πατήστε 3");
-        System.out.println("Άν έχετε απώλεια γεύσης πατήστε 4");
-        System.out.println("Άν έχετε απώλεια όσφρισης πατήστε 5");
-        System.out.println("Άν έχετε λαιμόπονο πατήστε 6");
-        System.out.println("Άν έχετε πονοκέφαλο πατήστε 7");
-        System.out.println("Άν έχετε δύσποια πατήστε 8");
-        System.out.println(
-            "Άν δεν επιθυμείτε να προσθέσετε κάποιο σύμπτωμα πατήστε 0");
-        int choice = -1;
-        while (choice < 0 || choice > EIGHT) {
-          try {
-            choice = sc.nextInt();
-
-          } catch (InputMismatchException ex) {
-            sc.next();
-            System.out.println("Μη έγκυρη τιμή");
-          }
-// yparxi alli ejairesi???
-          if (choice < 0 || choice > EIGHT) {
-            System.out.println("Εισάγετε επιλογή [0-8]");
-          }
-
-        }
-        switch (choice) {
-        case 0:
+    Scanner sc = null;
+    try {
+      sc = new Scanner(System.in);
+      String answer = null;
+      boolean flag = true;
+      while (flag) {
+        try {
+          answer = sc.next();
+          flag = false;
+        } catch (Exception e) {
           flag = true;
-          break;
-        case 1:
-          countsymptoma = countsymptoma + 2; // Σε κάποια συμπτώματα
-                                             // προστίθονται δύο γιατι θεωρείται
-                                             // οτι
-                                             // έχουν μεγαλύτερη βαρύτητα
-          break;
-        case 2:
-          countsymptoma = countsymptoma + 2;
-          break;
-        case THREE:
-          countsymptoma = countsymptoma + 1;
-          break;
-        case FOUR:
-          countsymptoma = countsymptoma + 2;
-          break;
-        case FIVE:
-          countsymptoma = countsymptoma + 2;
-          break;
-        case SIX:
-          countsymptoma = countsymptoma + 1;
-          break;
-        case SEVEN:
-          countsymptoma = countsymptoma + 1;
-          break;
-        case EIGHT:
-          countsymptoma = countsymptoma + 1;
-          break;
-        default:
-          break;
+          System.out.println("Εισάγετε (ΝΑΙ/ΟΧΙ) ");
         }
-        // ΝΑ ΞΑΝΑ ΕΛΕΓΞΟΥΜΕ ΤΗ SWITCH
       }
-      if (countsymptoma == 0) {
-        System.out.println("Δεν υπάρχει λόγος ανησυχίας");
-      } else if (countsymptoma < THREE) {
-        System.out.println("Παρακαλώ επικοινωνίστε με τον γιατρό σας και "
-            + "μέινετε προληπτικά σπιτι και να είστε σε επιφυλακή");
+      if (answer.equalsIgnoreCase("ΟΧΙ")) {
+        flag = false;
+        int countsymptoma = 0;
+        while (!flag) {
+          System.out.println("Άν έχετε πυρετό πατήστε 1");
+          System.out.println("Άν έχετε βήχα πατήστε 2");
+          System.out.println("Άν αισθάνεστε κόπωση πατήστε 3");
+          System.out.println("Άν έχετε απώλεια γεύσης πατήστε 4");
+          System.out.println("Άν έχετε απώλεια όσφρισης πατήστε 5");
+          System.out.println("Άν έχετε λαιμόπονο πατήστε 6");
+          System.out.println("Άν έχετε πονοκέφαλο πατήστε 7");
+          System.out.println("Άν έχετε δύσποια πατήστε 8");
+          System.out.println(
+              "Άν δεν επιθυμείτε να προσθέσετε κάποιο σύμπτωμα πατήστε 0");
+          int choice = -1;
+          while (choice < 0 || choice > EIGHT) {
+            try {
+              choice = sc.nextInt();
+
+            } catch (InputMismatchException ex) {
+              sc.next();
+              System.out.println("Μη έγκυρη τιμή");
+            }
+            if (choice < 0 || choice > EIGHT) {
+              System.out.println("Εισάγετε επιλογή [0-8]");
+            }
+
+          }
+          switch (choice) {
+          case 0:
+            flag = true;
+            break;
+          case 1:
+            countsymptoma = countsymptoma + 2; // Σε κάποια συμπτώματα
+                                               // προστίθονται δύο γιατι
+                                               // θεωρείται
+                                               // οτι
+                                               // έχουν μεγαλύτερη βαρύτητα
+            break;
+          case 2:
+            countsymptoma = countsymptoma + 2;
+            break;
+          case THREE:
+            countsymptoma = countsymptoma + 1;
+            break;
+          case FOUR:
+            countsymptoma = countsymptoma + 2;
+            break;
+          case FIVE:
+            countsymptoma = countsymptoma + 2;
+            break;
+          case SIX:
+            countsymptoma = countsymptoma + 1;
+            break;
+          case SEVEN:
+            countsymptoma = countsymptoma + 1;
+            break;
+          case EIGHT:
+            countsymptoma = countsymptoma + 1;
+            break;
+          default:
+            break;
+          }
+          // ΝΑ ΞΑΝΑ ΕΛΕΓΞΟΥΜΕ ΤΗ SWITCH
+        }
+        if (countsymptoma == 0) {
+          System.out.println("Δεν υπάρχει λόγος ανησυχίας");
+        } else if (countsymptoma < THREE) {
+          System.out.println("Παρακαλώ επικοινωνίστε με τον γιατρό σας και "
+              + "μέινετε προληπτικά σπιτι και να είστε σε επιφυλακή");
+        } else {
+          System.out.println("Παρακαλώ επικοινωνίστε το συντομότερο με το "
+              + "γιατρό σας, πιθανως να εχετε Covid-19");
+        }
       } else {
-        System.out.println("Παρακαλώ επικοινωνίστε το συντομότερο με το "
-            + "γιατρό σας, πιθανως να εχετε Covid-19");
+        System.out.println("Τέλεια! Να έχετε μια καλή μέρα και να προσέχετε!");
       }
-    } else {
-      System.out.println("Τέλεια! Να έχετε μια καλή μέρα και να προσέχετε!");
+    } finally {
+      if (sc != null) {
+        sc.close();
+      }
     }
   }
 }
